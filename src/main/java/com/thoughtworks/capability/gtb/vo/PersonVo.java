a0efbb5a736aeb52a906ba5b0b34563ff5826788
CH <1925480119@qq.com>
2020-11-08 23:33:56 +0800

    [chen] answerProblem2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +14,11 @@ public class PersonVo {
   private String id;
   private Integer age;
   private String name;
+  @JsonIgnore
   private String hobby;
+
+  @JsonGetter("age")
+  public int getTheAge() {
+    return age == null ? 0 : age;
+  }
 }
